72c5fcfb2398da280ee21ff5736c03cf5119966b
victor <vmoso2002@gmail.com>
2023-04-23 21:08:53 +0200

    front hecho terminado y completo

---
+++ trabajo/front/codigosJS/registro.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const checkbox1 = document.getElementById("checkbox1");
+    const checkbox2 = document.getElementById("checkbox2");
+    var nuevosCampos = document.getElementById('nuevosCampos');
+
+    checkbox2.addEventListener('change', function() {
+        if (checkbox2.checked) {
+        nuevosCampos.style.display = 'block';
+        } else {
+        nuevosCampos.style.display = 'none';
+        }
+    });
+
+
+    checkbox1.addEventListener('change', function() {
+        if (checkbox1.checked) {
+        nuevosCampos.style.display = 'none';
+        } else {
+        nuevosCampos.style.display = 'none';
+        }
+    });
+    
+    checkbox1.onclick = () => {
+      if (checkbox1.checked) {
+        checkbox2.checked = false;
+      }
+    };
+    
+    checkbox2.onclick = () => {
+      if (checkbox2.checked) {
+        checkbox1.checked = false;
+      }
+    };
+  });
+
