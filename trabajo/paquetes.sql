3976cdcad8ee45ed1d98fdd8d2e10283e4ff621b
alemale2001 <79916469+alemale2001@users.noreply.github.com>
2023-04-28 11:18:23 +0200

    Update paquetes.sql

--- trabajo/paquetes.sql
+++ trabajo/paquetes.sql
@@ -42,8 +42,8 @@ CREATE OR REPLACE PACKAGE mipa AS
         nadultos      IN NUMBER,
         nalojamientos IN NUMBER,
         id_camp       IN NUMBER,
-        fechain       IN DATE,
-        fechafin      IN DATE
+        fechain       IN VARCHAR2,
+        fechafin      IN VARCHAR2
     ) RETURN NUMBER;
 
     PROCEDURE eliminareserva (
@@ -221,8 +221,8 @@ CREATE OR REPLACE PACKAGE BODY mipa AS
         nadultos      IN NUMBER,
         nalojamientos IN NUMBER,
         id_camp       IN NUMBER,
-        fechain       IN DATE,
-        fechafin      IN DATE
+        fechain       IN VARCHAR2,
+        fechafin      IN VARCHAR2
     ) RETURN NUMBER IS
 
         preciofinal   NUMBER;
@@ -231,6 +231,8 @@ CREATE OR REPLACE PACKAGE BODY mipa AS
         --numero_dias   INTERVAL DAY TO SECOND := fechafin - fechain;
         dias          NUMBER;
         preciocamping NUMBER;
+        fechaini        DATE;
+        fechafinal      DATE;
     BEGIN
         SELECT
             ref(cli)
@@ -248,6 +250,9 @@ CREATE OR REPLACE PACKAGE BODY mipa AS
         WHERE
             camp.id = id_camp;
 
+        fechaini:=TO_DATE(fechain,'YYYY-MM-DD');
+        fechafinal:=TO_DATE(fechafin,'YYYY-MM-DD');
+
         INSERT INTO reservas (
             refcliente,
             refcamping,
@@ -262,8 +267,8 @@ CREATE OR REPLACE PACKAGE BODY mipa AS
             nninos,
             nadultos,
             nalojamientos,
-            fechain,
-            fechafin
+            fechaini,
+            fechafinal
         );
 
         SELECT
@@ -274,7 +279,7 @@ CREATE OR REPLACE PACKAGE BODY mipa AS
         WHERE
             camp.id = id_camp;
 
-        dias := trunc(fechafin-fechain);
+        dias := trunc(fechafinal-fechaini);
     
         preciofinal := ( ( preciocamping * 0.5 * nninos + preciocamping * nadultos ) * nalojamientos ) * ( dias );
 
@@ -285,18 +290,52 @@ END mipa;
 /
 
 DECLARE
-CURSOR mickey IS
-SELECT * FROM (select arraycampings from clientes where clientes.id=3) ;
+cliente_ref   REF tipocliente;
+camping_ref   REF tipocamping;
+
 
 BEGIN
-    for i in 1..mickey.count loop
-     dbms_output.put_line(mickey(i));
-     end loop;
+   
 
     --mipa.creacliente('Alemale', 'Alejandro', 'Jimenez', 'Garcia','aleelmaquina@gmail.es' ,'12345678');
     --mipa.creaanunciante('KBasilisk', 'Alejandro', 'Jimenez', 'Garcia','aleelmaquina@gmail.es' ,'12345678',1234567,'VIVAC entertaiment');
     --mipa.creaanuncio('KBasilisk', '27-MAR-2023', '28-MAR-2023', 1);
-    --DBMS_output.put_line('PRECIO:'|| mipa.creareserva('Tupu', 2, 2, 2, 1, '27-MAY-2023', '28-MAY-2023'));
+    --DBMS_output.put_line('PRECIO:'|| mipa.creareserva('Papanatas', 2, 2, 2, 1, '2023-03-12', '2023-03-13'));
+    
+    /*SELECT
+            ref(cli)
+        INTO cliente_ref
+        FROM
+            clientes cli
+        WHERE
+            cli.usuario = 'Papanatas';
+
+                SELECT
+            ref(camp)
+        INTO camping_ref
+        FROM
+            campings camp
+        WHERE
+            camp.id = 1;
+    
+    INSERT INTO reservas (
+            refcliente,
+            refcamping,
+            numninos,
+            numadultos,
+            numalojamientos,
+            fechaini,
+            fechafin
+        ) VALUES (
+            cliente_ref,
+            camping_ref,
+            2,
+            2,
+            2,
+            '12/03/23',
+            '13/03/23'
+        );
+        */
     --mipa.eliminareserva('Alemale',1);
      --cursoralgo:=mipa.getreservas(3);
      
