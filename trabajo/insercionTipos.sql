609e4296a56bf7f9592e67681c98a654322ea153
Victor Moreno <70269080+victxrms@users.noreply.github.com>
2023-05-01 21:15:54 +0200

    faltaban comillas

--- trabajo/insercionTipos.sql
+++ trabajo/insercionTipos.sql
@@ -9,10 +9,10 @@ INSERT INTO SITIOS (nombre, comarca) VALUES ('Kandersteg', 'Bern');
 INSERT INTO SITIOS (nombre, comarca) VALUES ('Griebal', 'La Ribagorza');
 INSERT INTO SITIOS (nombre, comarca) VALUES ('Bermejales', 'Alhama');
 
-INSERT INTO CAMPINGS (fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (1, 1, 2, 'Privado', 'Hace mucho calor pero te divertiras', 120, 'Ducha, piscina, cocina completa', 5);
-INSERT INTO CAMPINGS (fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (2, 2, 4, 'Privado', 'Crearas recuerdos inolvidables', 700, 'Duchas, Piscina, Catering, Rutas, Montañismo', 25);
-INSERT INTO CAMPINGS (fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (3, 3, 3, 'Publico', 'Conoceras gente increible', 500, 'Duchas, Piscina, Catering, Zonas de acampada', 15);
-INSERT INTO CAMPINGS (fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (4, 4, 5, 'Publico', 'Estaras con gente de toda España', 200, 'Duchas, Piscina, Cocina equipada, Lagos', 5);
+INSERT INTO CAMPINGS (id, fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (1, 1, 2, 'Privado', 'Hace mucho calor pero te divertiras', 120, 'Ducha, piscina, cocina completa', 5);
+INSERT INTO CAMPINGS (id, fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (2, 2, 4, 'Privado', 'Crearas recuerdos inolvidables', 700, 'Duchas, Piscina, Catering, Rutas, Montañismo', 25);
+INSERT INTO CAMPINGS (id, fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (3, 3, 3, 'Publico', 'Conoceras gente increible', 500, 'Duchas, Piscina, Catering, Zonas de acampada', 15);
+INSERT INTO CAMPINGS (id, fksitio, fktipo, status, descripcion, capacidad, servicios, precio) VALUES (4, 4, 5, 'Publico', 'Estaras con gente de toda España', 200, 'Duchas, Piscina, Cocina equipada, Lagos', 5);
 
 INSERT INTO CLIENTES (id, usuario, nombre, correo, telefono, arraycampings) VALUES (id_usuario.nextval, 'victxrms', TIPONOMBRE('Victor', 'Moreno', 'Sola'), 'vmoso2002@gmail.com', 655464789, TIPOARRAYCAMPINGS());
 INSERT INTO CLIENTES (id, usuario, nombre, correo, telefono, arraycampings) VALUES (id_usuario.nextval, 'kBasilisk', TIPONOMBRE('Alejandro', 'Jimenez', 'Garcia'), 'kbasliskelmejor@gmail.com', 643569874, TIPOARRAYCAMPINGS());
